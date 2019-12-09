2137a5dd168917949c90f39c4ce0b72689f7f886
Mike Mueller <mikemuellerware@gmail.com>
2019-12-08 21:34:19 -0500

    day 1 part 2

--- solution.rb
+++ solution.rb
@@ -1,7 +1,20 @@
 data = File.read("./data.txt")
 
+def calculateFuelReqForFuel(fuel) 
+    total = 0
+    fuelForFuel = fuel/3 - 2
+
+    while fuelForFuel > 0
+        total += fuelForFuel
+        fuelForFuel = fuelForFuel/3 -2
+    end
+    return total
+end
+
 total = data.split("\n").map(&:to_i).reduce(0) do |sum, num| 
-    sum += num/3 - 2
+    fuelReqForModule = num/3 - 2
+    fuelReqForFuel = calculateFuelReqForFuel(fuelReqForModule)
+    sum += fuelReqForModule + fuelReqForFuel
 end
 
 puts total
